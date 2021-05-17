839aa014362a84b8689c924a7acb966939777203
Kai Millett <RC0041917@student.riversidecollege.ac.uk>
2021-05-17 15:48:19 +0100

    Cuboid

--- Assignment 2/Form1.Designer.cs
+++ Assignment 2/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(768, 400);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Cuboid";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -63,7 +63,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(768, 400);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Sphere";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
@@ -73,7 +73,7 @@
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage3.Size = new System.Drawing.Size(768, 400);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.Text = "Cone";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // Form1
